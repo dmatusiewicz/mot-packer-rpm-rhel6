73e6f56b5629881343fed31e372cdc92a151e526
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2016-02-02 12:11:36 +0000

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # packer-rpm-rhel6
-Creates packer rpm. 
+Creates packer rpm.
+Very simple and static script. Just to solve my problem quick.
+** first repo on github
---
+++ build-packer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+NAME='packer.io'
+URL='https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip'
+VERSION=${BUILD}
+curl -k -L -o ${URL##*/} $URL || {
+    echo $"URL or version not found!" >&2
+    exit 1
+}
+
+mkdir -p packer/target/opt/packer
+unzip -qq ${URL##*/} -d packer/target/opt/packer
+
+/opt/ruby22/bin/fpm -s dir -t rpm -f \
+	-C packer/target \
+	-v ${BUILD} \
+	-n ${NAME} \
+	-p packer/target \
+	-a amd64 \
+        -v ${BUILD} \
+        --rpm-ignore-iteration-in-dependencies \
+        --description "Simple Packer RPM package for RedHat Enterprise Linux 6" \
+        --url "https://github.com/dmatusiewicz/packer-rpm-rhel6" \
+        opt/packer
+
